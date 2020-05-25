a3fb13c11ca5388d9c973ea53996129d03ea53d6
Krlstch <karolstach1999@gmail.com>
2020-05-25 23:01:02 +0200

    Add files via upload

--- Observer.py
+++ Observer.py
@@ -1,12 +1,17 @@
+import random
+from Enemy import Enemy
+
+
 class Observer:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.bullets = set()
+        self.enemies = set()
         self.char = None
-
-    def remove_bullet(self, bullet):
-        self.bullets.remove(bullet)
+        self.enemy_time = 0
+        self.max_enemy_time = 120
+        self.min_enemy_time = 60
 
     def add_bullet(self, bullet):
         self.bullets.add(bullet)
@@ -14,10 +19,47 @@ class Observer:
     def add_char(self, char):
         self.char = char
 
+    def add_enemy(self, enemy):
+        self.enemies.add(enemy)
+
     def update_bullets(self):
         deleted_bullets = []
         for bullet in self.bullets:
             if not bullet.update_pos():
                 deleted_bullets.append(bullet)
         for bullet in deleted_bullets:
+            self.delete_bullet(bullet)
+
+    def delete_bullet(self, bullet):
+        if bullet in self.bullets:
             self.bullets.remove(bullet)
+
+    def update_enemies(self):
+        deleted_enemies = []
+        deleted_bullets = []
+        for enemy in self.enemies:
+            enemy.update_direction((self.char.x, self.char.y))
+            hit_bullet = enemy.update_pos(self.bullets)
+            if hit_bullet is not None:
+                deleted_enemies.append(enemy)
+                deleted_bullets.append(hit_bullet)
+        for enemy in deleted_enemies:
+            self.delete_enemies(enemy)
+        for bullet in deleted_bullets:
+            self.delete_bullet(bullet)
+
+    def delete_enemies(self, enemy):
+        if enemy in self.enemies:
+            self.enemies.remove(enemy)
+
+    def update_spawn_time(self):
+        if self.enemy_time > 0:
+            self.enemy_time -= 1
+        else:
+            self.spawn_enemy()
+            self.enemy_time = random.randint(self.min_enemy_time, self.max_enemy_time)
+
+    def spawn_enemy(self):
+        x = random.choice([0, self.x])
+        y = random.choice([0, self.y])
+        enemy = Enemy(x, y, self)
--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import pygame
 from Char import Char
 from Observer import Observer
+from Enemy import Enemy
 
-observer = Observer(800, 600)
-pygame.init()
-gameDisplay = pygame.display.set_mode((observer.x, observer.y))
-pygame.display.set_caption('Alien Shooter')
 
-char = Char(observer)
+def draw(gameDisplay, char, bullets, enemies):
+    gameDisplay.fill((0, 0, 0))
+    pygame.draw.circle(gameDisplay, (0, 255, 0), (int(char.x), int(char.y)), 10, 10)
+    for bullet in bullets:
+        pygame.draw.rect(gameDisplay, (255, 0, 0), (int(bullet.x), int(bullet.y), 1, 1))
+    for enemy in enemies:
+        pygame.draw.circle(gameDisplay, (255, 0, 0), (int(enemy.x), int(enemy.y)), 10, 10)
+    pygame.display.update()
 
-run = True
-while run:
-    pygame.time.delay(100)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
 
+def update(char, observer):
+    char.update_direction(pygame.mouse.get_pos())
+    char.update_reload()
+    observer.update_bullets()
+    observer.update_enemies()
+
+
+def spawn_enemy(observer):
+    observer.update_spawn_time()
+
+
+def take_input():
     keys = pygame.key.get_pressed()
     mouse = pygame.mouse.get_pressed()
 
@@ -34,16 +44,25 @@ while run:
     if mouse[0]:
         char.shoot()
 
-    char.update_direction(pygame.mouse.get_pos())
-    char.update_reload()
-    observer.update_bullets()
 
-    gameDisplay.fill((0, 0, 0))
+if __name__ == "__main__":
+    observer = Observer(800, 600)
+    pygame.init()
+    gameDisplay = pygame.display.set_mode((observer.x, observer.y))
+    pygame.display.set_caption('Alien Shooter')
 
-    pygame.draw.rect(gameDisplay, (0, 255, 0), (char.x, char.y, 10, 10))
-    for bullet in observer.bullets:
-        pygame.draw.rect(gameDisplay, (255, 0, 0), (bullet.x, bullet.y, 1, 1))
-    pygame.display.update()
+    char = Char(observer)
+    run = True
+    while run:
+        pygame.time.delay(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-pygame.quit()
-quit()
+        take_input()
+        spawn_enemy(observer)
+        update(char, observer)
+        draw(gameDisplay, char, observer.bullets, observer.enemies)
+
+    pygame.quit()
+    quit()
